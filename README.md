acf413881a66d38027725fb4585308311439c73a
unknown <unknown@example.com>
2023-04-11 17:48:58 +0800

    修改readMe

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # miniApp
 微信小程序基于WanAndroid的api示例
+
+# 文章地址
+https://juejin.cn/post/7220662514907136037
--- app.js
+++ app.js
@@ -10,7 +10,7 @@ App({
   },
 
   /**
-   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次） 
    */
   onLaunch: function() {
     this.getMenuInfo();
